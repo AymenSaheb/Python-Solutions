7a975a7f4b0a7fc14ec58cd1e6deaf37574b8398
Aymen Saheb <sahebaymenmohamed@gmail.com>
2024-12-13 19:34:18 +0100

    added exercice4

---
+++ exercice4.py
@@ -0,0 +1,8 @@
+age1=int(input("Please type in the age of the first person :"))
+age2=int(input("Please type in the age of the second person :"))
+if age1>age2:
+    print(f"the older age is : {age1}")
+elif age1==age2:
+    print("Both people are the same age!")
+else: 
+    print(f"the older age is : {age2}") 
